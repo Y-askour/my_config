64523876f323e96755ee4a7a4293f28bcaa6dcfb
Younes Askour <pausedarkf@gmail.com>
2023-01-31 19:14:40 +0100

    my_config

--- test.sh
+++ test.sh
@@ -5,37 +5,37 @@ homeDir="$baseDir/home/user"
 
 
 printf "### Updating System ..."
-doas xbps-install -Syu
+sudo xbps-install -Syu
 printf "### Install X-server ..."
-doas xbps-install -Sy xorg-server xinit libX11-devel libXft-devel libXinerama-devel glib-devel font-inconsolata-otf
-doas xbps-install -Sy xorg
+sudo xbps-install -Sy xorg-server xinit libX11-devel libXft-devel libXinerama-devel glib-devel font-inconsolata-otf
+sudo xbps-install -Sy xorg
 printf "### Install Builiding toolsxhkd sssssss ..."
-doas xbps-install -Sy git gcc mkae pkg-config
+sudo xbps-install -Sy git gcc mkae pkg-config
 printf "### Install man pages ..."
-doas xbps-install -Sy man-pages-devel
+sudo xbps-install -Sy man-pages-devel
 
 
 
 printf "### Install tools for for suckless surf ..."
-doas xbps-install -Sy webkit2gtk-devel gcr-devel
+sudo xbps-install -Sy webkit2gtk-devel gcr-devel
 
 printf "### Install tools for for suckless to play video ..."
-doas xbps-install -Sy gst-libav gst-plugins-good1
+sudo xbps-install -Sy gst-libav gst-plugins-good1
 
 printf "### Install multimedia ..."
-doas xbps-install -Sy mpd ffmpeg mpv youtube-dl
+sudo xbps-install -Sy mpd ffmpeg mpv youtube-dl
 
 printf "### Diagnostics ..."
-doas xbps-install -Sy iotop htop
+sudo xbps-install -Sy iotop htop
 
 printf "### Install General Tools ..."
-doas xbps-install -Sy groff sxhkd zathura-pdf-mupdf nvim rsync firefox alsa-utils xz ImageMagick pass
+sudo xbps-install -Sy groff sxhkd zathura-pdf-mupdf nvim rsync firefox alsa-utils xz ImageMagick pass
 
 printf "### Install e-Mail Tools"
 doad xbps-install -Sy neomutt msmtp
 
 printf "### Install FileSystems support ... "
-doas xbps-install -Sy fuse-exfat
+sudo xbps-install -Sy fuse-exfat
 
 
 # .files
@@ -47,10 +47,10 @@ cp -r $homeDir/. ~/
  do
 	 cd $HOME/suckless/$sucklessProgram
 	 git checkout master
-	 doas make install
+	 sudo make install
 	 make clean
 done
 
 ## PC ## 
 printf " ### Install nvidia ..."
-doas xbps-install -Sy nvidia-opencl
+sudo xbps-install -Sy nvidia-opencl
